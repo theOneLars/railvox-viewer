277fb1b20dbc9a44245daa89c3cfa886964abd84
The one Lars <none>
2025-11-18 09:15:54 +0100

    minify webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,7 @@
 module.exports = {
   resolve: {
     fallback: {
-      "util": require.resolve("util/"),
-      "path": require.resolve("path-browserify"),
       "stream": require.resolve("path-browserify"),
-      "events": require.resolve("events/")
     }
   }
 };
